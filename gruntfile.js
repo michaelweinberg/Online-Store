354bed91c7d8d11b4f68f225874ac5861e4a3132
Michael Weinberg <weinberg4@gmail.com>
2015-06-01 14:54:43 -0700

    Built protosite into handlebars

--- gruntfile.js
+++ gruntfile.js
@@ -1,55 +1,21 @@
 module.exports = function(grunt){
-	var npm = ["grunt-contrib-watch", "grunt-concurrent", "grunt-nodemon", "grunt-contrib-less", "grunt-contrib-copy",'grunt-contrib-imagemin',"grunt-autoprefixer"];
+	var npm = ["grunt-contrib-watch", "grunt-contrib-less"];
 	npm.forEach(grunt.loadNpmTasks);
 	
-	grunt.registerTask("default", ["autoprefixer","concurrent"]);
+	grunt.registerTask("default", ["less","watch"]);
 	
 	grunt.initConfig({
-		copy:{
-			 files: {
-			    cwd: 'src/images/',  // set working folder / root to copy
-			    src: '**',           // copy all files and subfolders
-			    dest: 'build/images',    // destination folder
-			    expand: true           // required when using cwd
-  			}
-		},
-		concurrent:{
-			dev:{
-				tasks:["watch","nodemon","imagemin","less"],
-				options:{
-					logConcurrentOutput:true
-				}
-			}
-		},
+	
+	
 		watch:{
 			options:{
 				livereload:true,
 				
 			},
-			
-			prefix:{
-				files:"public/css/style.css",
-				tasks:["autoprefixer"]
-			},
-			images:{
-				files:"src/images/**",
-				tasks:["copy"]
-			},
-			html:{
-				files:"templates/**",
-				 options: {
-			      reload: true
-			    }
-			},
 			less:{
-				files:"src/less/**/*.less",
+				files:"assets/less/**/*.less",
 				tasks:["less"]
 			}
-		},
-		nodemon:{
-				dev:{
-					script:"index.js"
-				}
 		},
 		less: {
       		dev: {
@@ -57,32 +23,6 @@ module.exports = function(grunt){
 		          "public/css/style.css": "src/less/*.less"
 		        }
       		}
-    	},
-
-	    imagemin: {                          
-		    dynamic: {
-		    	options: {                       // Target options 
-		        optimizationLevel: 3
-		      },                         // Another target 
-		      files: [{
-		        expand: true,                  // Enable dynamic expansion 
-		        cwd: 'src/images/',                   // Src matches are relative to this path 
-		        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match 
-		        dest: 'public/images/'                  // Destination path prefix 
-		      }]
-		    }
-	  	},
-	  	autoprefixer: {
-			dev: {
-				expand: true,
-				flatten: true,
-				src: "public/css/**/*.css"
-			}
-		}
-    
-    
-    
-    
-    
+    	}
 	});
 };
--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 var hapi = require("hapi");
 var server = new hapi.Server();
+var handlebars = require("handlebars");
 server.connection({ port: 8000 });
 server.start();
 
-//view config
-server.views({
-  path: "templates",
-  engines: {
-    html: require("handlebars")
-  },
-  isCached: false
-});
+
 
 //home page
 server.route({
   method: "GET",
   path: "/",
   handler: function(req, reply) {
-    reply.view("index");
+    reply.view("index",{
+			title:"Home Page"
+		});
   }
+});
+
+server.route({
+	method: "GET",
+	path: "/product",
+	handler: function(request, reply){
+		reply.view("/product",{
+			title:"Product Page"
+		});
+	}
 });
 
 
@@ -31,3 +37,28 @@ server.route({
     }
   }
 });
+
+server.route({
+	method:"GET",
+	path: "/{index}",
+	handler:function(request, reply){
+		fs.readFile("products.json", "utf8", function(err,data){
+			var products = JSON.parse(data);
+			var item = products.makers[request.params.index];
+			reply.view("/product",{
+				loop: item
+			});
+		});
+	}
+});
+
+server.views({
+	path:"templates",
+	engines:{
+		html:require("handlebars")
+	},
+	layoutPath: "layouts",
+	layout: "default",
+	isCached:false
+});
+
