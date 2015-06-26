f7e92459ec8beb043179fa66d6cd5afc0c261c35
Michael Weinberg <weinberg4@gmail.com>
2015-06-26 08:21:17 -0700

    @thomaswilburn Please see email

--- handlers/product.js
+++ handlers/product.js
@@ -14,9 +14,6 @@ module.exports = function(request, reply){
 			
 		db.getAllReviews(function(err, reviews) {
 			
-		// console.log("product handler", model.toJSON());
-		// console.log(id);
-		// console.log(index);
 			model.set("id",id);
 			model.load(function(err){
 				var data;
